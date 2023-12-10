5dc13f479a71d0e912b2d21c325d0ee69074007f
sajib689 <jibonraja11@gmail.com>
2023-12-10 20:23:08 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Install react Js.
+2. Install react router dom.
+3. Install Tailwind css.
+4. npm install react-responsive-carousel.
---
+++ src/Pages/Home/Featured/Featured.jsx
@@ -0,0 +1,17 @@
+import SectionTitle from "./../../../Components/SectionTitle/SectionTitle";
+import featuredImg from '../../../assets/home/featured.jpg'
+const Featured = () => {
+  return (
+    <div>
+      <SectionTitle
+        heading={"FROM OUR MENU"}
+        subHeading={"---Check it out---"}
+      ></SectionTitle>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
