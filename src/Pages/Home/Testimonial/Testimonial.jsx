e9c5d73ab6022868cb803f5763170d7d6a69e622
sajib689 <jibonraja11@gmail.com>
2023-12-13 19:09:21 +0600

    star design success

--- src/Pages/Home/Testimonial/Testimonial.jsx
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -9,7 +9,6 @@ import { Rating } from "@smastrom/react-rating";
 import '@smastrom/react-rating/style.css'
 const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
-  const [rating, setRating] = useState(0);
 
   console.log(reviews);
   useEffect(() => {
@@ -27,13 +26,12 @@ const Testimonial = () => {
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
           {reviews.map((review) => (
             <SwiperSlide key={review._id}>
-              <div className="text-center m-24">
+              <div className="text-center flex flex-col items-center mx-24 my-16">
                 <Rating
                   style={{ maxWidth: 180 }}
-                  value={rating}
-                  onChange={setRating}
+                  value={review.rating}
                 />
-                <p>{review.details}</p>
+                <p className="py-8">{review.details}</p>
                 <h3 className="text-2xl text-orange-400">{review.name}</h3>
               </div>
             </SwiperSlide>
