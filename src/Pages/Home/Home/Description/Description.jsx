929c7a983406111725fb1227e6b9797c24a844ff
sajib689 <jibonraja11@gmail.com>
2023-12-13 21:51:08 +0600

    add a new section descriptiopn

---
+++ src/Pages/Home/Home/Description/Description.jsx
@@ -0,0 +1,19 @@
+import './Description.css'
+const Description = () => {
+  return (
+    <section className="description-bg">
+      <div className="hero min-h-screen">
+        <div className="hero-content text-center bg-white px-80 py-12">
+          <div className="max-w-md">
+            <h1 className="text-5xl uppercase">Bistro Boss</h1>
+            <p className="py-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Description;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,15 +3,18 @@ import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonial from "../Testimonial/Testimonial";
+import Description from "./Description/Description";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Category></Category>
+            <Description></Description>
             <PopularMenu></PopularMenu>
             <Featured></Featured>
             <Testimonial></Testimonial>
+            
         </div>
     );
 };
