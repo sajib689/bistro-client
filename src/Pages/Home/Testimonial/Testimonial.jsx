89e77a9347c400220b1a6e17537d25a8ee1a70fe
sajib689 <jibonraja11@gmail.com>
2023-12-13 16:59:07 +0600

    reviews section add

--- src/Pages/Home/Testimonial/Testimonial.jsx
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -1,13 +1,32 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
 const Testimonial = () => {
+    const [reviews, setReviews] = useState([])
+    useEffect( () => {
+        fetch('reviews.json')
+        .then(res => res.json())
+        .then(data => setReviews(data));
+    },[])
     return (
-        <section>
+        <section className="my-20">
             <SectionTitle 
             subHeading={'---What Our Clients Say---'}
             heading={'TESTIMONIALS'}
             >
         </SectionTitle>
+        <>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        {
+            reviews.map(review => console.log(review))
+        }
+      </Swiper>
+        </>
         </section>
     );
 };
