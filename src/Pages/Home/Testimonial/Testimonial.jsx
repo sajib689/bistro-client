856b9d39f8a1c59f4d2eb81ab940085ef0381566
sajib689 <jibonraja11@gmail.com>
2023-12-13 19:06:02 +0600

    review star add

--- src/Pages/Home/Testimonial/Testimonial.jsx
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -1,41 +1,47 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
 const Testimonial = () => {
-    const [reviews, setReviews] = useState([])
-    console.log(reviews);
-    useEffect( () => {
-        fetch('reviews.json')
-        .then(res => res.json())
-        .then(data => setReviews(data));
-    },[])
-    return (
-        <section className="my-20">
-            <SectionTitle 
-            subHeading={'---What Our Clients Say---'}
-            heading={'TESTIMONIALS'}
-            >
-        </SectionTitle>
-        <>
+  const [reviews, setReviews] = useState([]);
+  const [rating, setRating] = useState(0);
+
+  console.log(reviews);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <section className="my-20">
+      <SectionTitle
+        subHeading={"---What Our Clients Say---"}
+        heading={"TESTIMONIALS"}
+      ></SectionTitle>
+      <>
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-       
-        {
-            reviews.map(review =>  <SwiperSlide
-             key={review._id}>
-                <div className="text-center m-24">
-                    <p>{review.details}</p>
-                    <h3 className="text-2xl text-orange-400">{review.name}</h3>
-                </div>
-            </SwiperSlide>)
-        }
-      </Swiper>
-        </>
-        </section>
-    );
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="text-center m-24">
+                <Rating
+                  style={{ maxWidth: 180 }}
+                  value={rating}
+                  onChange={setRating}
+                />
+                <p>{review.details}</p>
+                <h3 className="text-2xl text-orange-400">{review.name}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    </section>
+  );
 };
 
 export default Testimonial;
