15a559978007ef733b799a4be0e57e1c02a8a5be
sajib689 <jibonraja11@gmail.com>
2023-12-14 19:19:34 +0600

    react helmat add for title

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,9 @@
+import {Helmet} from "react-helmet";
 
 const Menu = () => {
     return (
         <div>
+            <Helmet><title>Bistro | Menu</title></Helmet>
             <h1>Main menu</h1>
         </div>
     );
