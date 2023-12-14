af7523d8c2f4b56ecfdc76516a9d3fcfc12176b5
sajib689 <jibonraja11@gmail.com>
2023-12-14 22:44:36 +0600

    cover section add

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,13 @@
 import { Helmet } from "react-helmet";
-
+import Cover from "../../Shared/Cover/Cover";
+import img from '../../../assets/menu/banner3.jpg'
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Bistro | Menu</title>
       </Helmet>
-      <h1>Main menu</h1>
+      <Cover img={img} title={'OUR MENU'} description={'Would you like to try a dish?'}></Cover>
     </div>
   );
 };
---
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -0,0 +1,24 @@
+const Cover = ({img, title, description}) => {
+  return (
+    <section>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("${img}")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md bg-[rgba(21, 21, 21, 0.60)]">
+            <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+            <p className="mb-5 uppercase">
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cover;
