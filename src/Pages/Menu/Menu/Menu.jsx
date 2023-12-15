5d018d479044e3c259db3cdb1a9967bddb0a7524
sajib689 <jibonraja11@gmail.com>
2023-12-15 10:33:50 +0600

    install react paralax

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,19 @@
 import { Helmet } from "react-helmet";
 import Cover from "../../Shared/Cover/Cover";
-import img from '../../../assets/menu/banner3.jpg'
+import img from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "./../../Home/PopularMenu/PopularMenu";
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Bistro | Menu</title>
       </Helmet>
-      <Cover img={img} title={'OUR MENU'} description={'Would you like to try a dish?'}></Cover>
+      <Cover
+        img={img}
+        title={"OUR MENU"}
+        description={"Would you like to try a dish?"}
+      ></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,8 +1,8 @@
-const Cover = ({img, title, description}) => {
+const Cover = ({ img, title, description }) => {
   return (
     <section>
       <div
-        className="hero min-h-screen"
+        className="hero h-[700px]"
         style={{
           backgroundImage: `url("${img}")`,
         }}
@@ -11,9 +11,7 @@ const Cover = ({img, title, description}) => {
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md bg-[rgba(21, 21, 21, 0.60)]">
             <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
-            <p className="mb-5 uppercase">
-              {description}
-            </p>
+            <p className="mb-5 uppercase">{description}</p>
           </div>
         </div>
       </div>
