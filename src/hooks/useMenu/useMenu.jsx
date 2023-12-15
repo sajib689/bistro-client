77119a8e0c6ebf6e718665d64b7a2c8a38e2f545
sajib689 <jibonraja11@gmail.com>
2023-12-15 10:56:31 +0600

    setLoading for data load easy

--- src/hooks/useMenu/useMenu.jsx
+++ src/hooks/useMenu/useMenu.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 
 const useMenu = () => {
     const [menu, setMenu] = useState([]);
+    const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch("menu.json")
       .then((res) => res.json())
       .then((data) => {
         setMenu(data);
+        setLoading(false)
       });
   }, []);
-  return [menu]
+  return [menu, loading]
 }
 
 export default useMenu;
