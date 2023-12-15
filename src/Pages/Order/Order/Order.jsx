dbb1fb4b88564c42ebf01e8521239b8e6d4b8aff
sajib689 <jibonraja11@gmail.com>
2023-12-15 19:11:00 +0600

    order page add tab index

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,13 +1,42 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
+import shopImg from "../../../assets/shop/banner2.jpg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { useState } from "react";
 
-const Order = ({img, title, description}) => {
-    return (
-        <div>
-            <Helmet><title>Bistro | Shop</title></Helmet>
-            <Cover img={img} title={title} description={description}></Cover>
-        </div>
-    );
+const Order = () => {
+    const [tabIndex, setTabIndex] = useState(0)
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro | Shop</title>
+      </Helmet>
+      <Cover
+        img={shopImg}
+        title={"OUR SHOP"}
+        description={"Would you like to try a dish?"}
+      ></Cover>
+      {/* tabs section */}
+      <Tabs
+        defaultIndex={tabIndex}
+        onSelect={(index) => setTabIndex(index)}
+        className="tabs tabs-boxed"
+      >
+        <TabList>
+          <Tab className="tab">Title 1</Tab>
+          <Tab className="tab">Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Order;
