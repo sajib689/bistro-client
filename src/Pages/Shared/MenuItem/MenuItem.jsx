e278fcda77cb2f6ddb22511e839241d4c15a2a8a
sajib689 <jibonraja11@gmail.com>
2023-12-15 13:04:22 +0600

    cover page some changes

--- src/Pages/Shared/MenuItem/MenuItem.jsx
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -2,13 +2,13 @@
 const MenuItem = ({item}) => {
     const {name, image, price, recipe} = item;
     return (
-        <div className="flex space-x-4">
+        <div className="flex space-x-2 items-center">
             <img style={{borderRadius: '0 200px 200px 200px'}} className='w-[100px]' src={image} alt="img" />
             <div>
-                <h3 className="uppercase">{name}............</h3>
+                <h3 className="uppercase">{name}........</h3>
                 <p>{recipe}</p>
             </div>
-            <p className="text-yellow-500">$ {price}</p>
+            <p className="text-yellow-500">${price}</p>
         </div>
     );
 };
