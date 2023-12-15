323720d2ebfd6f70dc940899b5c50661a3d8d2fd
sajib689 <jibonraja11@gmail.com>
2023-12-15 12:41:44 +0600

    still design menu page

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,9 @@
 import { Helmet } from "react-helmet";
 import Cover from "../../Shared/Cover/Cover";
 import img from "../../../assets/menu/banner3.jpg";
+import dessert from '../../../assets/menu/dessert-bg.jpeg';
+import salad from '../../../assets/menu/salad-bg.jpg'
+import pizza from '../../../assets/menu/pizza-bg.jpg'
 import useMenu from "../../../hooks/useMenu/useMenu";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuCategory from "../MenuCategory/MenuCategory";
@@ -16,17 +19,25 @@ const Menu = () => {
       <Helmet>
         <title>Bistro | Menu</title>
       </Helmet>
+      {/* main cover */}
       <Cover
         img={img}
         title={"OUR MENU"}
         description={"Would you like to try a dish?"}
       ></Cover>
+      {/* section title */}
       <SectionTitle
         subHeading={"---Don't miss---"}
         heading={"TODAY'S OFFER"}
       ></SectionTitle>
-
+    {/* offered section */}
     <MenuCategory items={offered}></MenuCategory>
+  
+    {/* desserts menu items section */}
+    <MenuCategory items={desserts}  img={dessert}
+        title={"DESSERTS"}
+        description={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+      ></MenuCategory>
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,11 @@
+import Cover from "../../Shared/Cover/Cover";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = ({ items }) => {
+const MenuCategory = ({ items, title, img,description }) => {
   return (
-    <div>
-      <div className="grid md:grid-cols-2 gap-10 mb-12">
+    <div className="mt-24">
+        {title && <Cover img={img} title={title} description={description}></Cover>}
+      <div className="grid md:grid-cols-2 gap-10 mb-12 mt-24">
         {items.map((item) => (
           <MenuItem item={item} key={item._id}></MenuItem>
         ))}
