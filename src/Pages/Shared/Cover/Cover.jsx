75a8758cb70e93565628750b801b162b47b3018f
sajib689 <jibonraja11@gmail.com>
2023-12-15 10:44:37 +0600

    cover desgin end

--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,21 +1,24 @@
+import { Parallax } from "react-parallax";
 const Cover = ({ img, title, description }) => {
   return (
-    <section>
-      <div
-        className="hero h-[700px]"
-        style={{
-          backgroundImage: `url("${img}")`,
-        }}
+    <>
+      <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={img}
+        bgImageAlt="the dog"
+        strength={-200}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md bg-[rgba(21, 21, 21, 0.60)]">
-            <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
-            <p className="mb-5 uppercase">{description}</p>
+        <div className="hero h-[700px]">
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md ">
+              <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+              <p className="mb-5 uppercase">{description}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </Parallax>
+    </>
   );
 };
 
