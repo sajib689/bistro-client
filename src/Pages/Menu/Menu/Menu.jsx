f1bbc43697c21525f1b70d169d1f45255a5775f8
sajib689 <jibonraja11@gmail.com>
2023-12-15 11:41:08 +0600

    add todays offered

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,16 @@
 import { Helmet } from "react-helmet";
 import Cover from "../../Shared/Cover/Cover";
 import img from "../../../assets/menu/banner3.jpg";
+import useMenu from "../../../hooks/useMenu/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 const Menu = () => {
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
   return (
     <div>
       <Helmet>
@@ -12,6 +21,12 @@ const Menu = () => {
         title={"OUR MENU"}
         description={"Would you like to try a dish?"}
       ></Cover>
+      <SectionTitle
+        subHeading={"---Don't miss---"}
+        heading={"TODAY'S OFFER"}
+      ></SectionTitle>
+
+    <MenuCategory items={offered}></MenuCategory>
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,15 @@
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items }) => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-2 gap-10 mb-12">
+        {items.map((item) => (
+          <MenuItem item={item} key={item._id}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
