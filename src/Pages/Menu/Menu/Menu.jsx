c726b9c8f0756f38f6abd984aa04984b5b053aa3
sajib689 <jibonraja11@gmail.com>
2023-12-15 10:55:05 +0600

    hooks add

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,6 @@
 import { Helmet } from "react-helmet";
 import Cover from "../../Shared/Cover/Cover";
 import img from "../../../assets/menu/banner3.jpg";
-import PopularMenu from "./../../Home/PopularMenu/PopularMenu";
 const Menu = () => {
   return (
     <div>
@@ -13,7 +12,6 @@ const Menu = () => {
         title={"OUR MENU"}
         description={"Would you like to try a dish?"}
       ></Cover>
-      <PopularMenu></PopularMenu>
     </div>
   );
 };
