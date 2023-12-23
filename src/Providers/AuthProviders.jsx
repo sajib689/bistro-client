25a4321d93b92777dc09a5b53ce1fd6844ec7d2f
sajib689 <jibonraja11@gmail.com>
2023-12-23 13:12:28 +0600

    authprovider add

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,11 +1,24 @@
-import { createContext, useState } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from './../Firebase/Firebase.config';
 export const AuthContext = createContext(null)
+const auth = getAuth(app)
 const AuthProviders = ({children}) => {
-    const [user, setuser] = useState(null)
+    const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
-    const authInfo = {
 
+    useEffect( () => {
+     const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unsubscribe()
+        }
+    },[])
+    const authInfo = {
+        user,
+        loading
     }
     return (
         <AuthContext.Provider value={authInfo}>
