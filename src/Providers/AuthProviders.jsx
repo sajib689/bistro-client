c9776f60139bd99912eb0463cb63cbbcc27bb55e
sajib689 <jibonraja11@gmail.com>
2023-12-23 17:05:29 +0600

    add logout system

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from './../Firebase/Firebase.config';
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
@@ -7,11 +7,16 @@ const AuthProviders = ({children}) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
     const createUser = (email, password) => {
-        setLoading(false)
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
     const signIn = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
+    }
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
     }
     useEffect( () => {
      const unsubscribe = onAuthStateChanged(auth, currentUser => {
