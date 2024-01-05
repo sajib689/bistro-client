f9b2a12807ae95434dabef9ceec830abd644ca09
sajib689 <jibonraja11@gmail.com>
2024-01-05 12:27:40 +0600

    google login system add in ui

--- src/Pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/Pages/Shared/SocialLogin/SocialLogin.jsx
@@ -1,11 +1,22 @@
+import { useContext } from "react";
 import { FaGoogle } from "react-icons/fa6";
+import { AuthContext } from "../../../Providers/AuthProviders";
 
 const SocialLogin = () => {
+    const {googleSign} = useContext(AuthContext)
+   
+    const handleGoogleSignIn = () => {
+        googleSign()
+        .then( result => {
+            const loggedInUser = result.user
+            console.log(`User ${loggedInUser}`)
+        })
+    }
   return (
     <div>
       <div className="divider"></div>
       <div className="w-full text-center">
-      <button className="btn btn-circle btn-outline">
+      <button onClick={handleGoogleSignIn} className="btn btn-circle btn-outline">
         <FaGoogle></FaGoogle>
       </button>
       </div>
